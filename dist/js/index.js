68f94e818416bf474088db54ebecbe0b9bef7652
Hong Baojin <13809735212@163.com>
2019-07-18 17:58:07 +0800

    modify README.md

--- dist/js/index.js
+++ dist/js/index.js
@@ -12,9 +12,10 @@ window.onload = function () {
   var contentList = document.querySelectorAll('#app .content > div');
   var guideItems = document.querySelectorAll('#app .guide-item');
   var profileLink = document.querySelector('#app .profile-link');
-  var lgoinOrRegister = document.querySelectorAll('.login-content > form > div');
+  var loginOrRegister = document.querySelectorAll('.login-content > form > div');
   var loginA = document.querySelectorAll('.login-header-title > a');
   var switchCircle = document.querySelector('.switch-circle');
+  var switchButton = document.querySelector('.switch-button');
   var switchText = document.querySelector('.switch-text');
   var goBack = document.querySelector('.go-back');
   var login = document.querySelector('#app > div');
@@ -46,4 +47,45 @@ window.onload = function () {
       contentList[this.index].className = 'on';
     });
   }
+
+  /* 切换手机号登录/密码登录 */
+  for (var i = 0; i < loginA.length; i++) {
+    loginA[i].index = i;
+    loginA[i].addEventListener('touchend', function () {
+      for (var i = 0; i < loginOrRegister.length; i++) {
+        loginA[i].className = '';
+        loginOrRegister[i].className = '';
+      }
+      this.className = 'on';
+      loginOrRegister[this.index].className = 'on';
+    });
+  }
+
+  /* 登录界面切换密码明/暗文 */
+  var isSwitch = false;
+  switchButton.addEventListener('touchend', function () {
+    if (!isSwitch) {
+      switchCircle.style.transform = 'translateX(27px)';
+      this.className = 'switch-button on';
+      switchText.innerText = 'abc';
+    } else {
+      switchCircle.style.transform = 'translateX(0)';
+      this.className = 'switch-button off';
+      switchText.innerText = '...';
+    }
+    isSwitch = !isSwitch;
+  });
+
+  /* 控制登录界面的 on/off */
+  profileLink.addEventListener('touchend', function () {
+    login.className = 'on';
+  });
+  goBack.addEventListener('touchend', function () {
+    login.className = 'off';
+  });
+
+  /* 点击首页的登录注册 跳转到登录注册 */
+  headerLogin.addEventListener('touchend', function () {
+    login.className = 'on';
+  });
 };
